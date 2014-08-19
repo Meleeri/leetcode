beaa4f3be5b5fa6c5e4adafc7c2861693b46763a
lijiafan <li.jiafan713@gmail.com>
2014-08-19 18:49:59 +0800

    added Add Two Numbers

--- Add_Two_Numbers/main.cpp
+++ Add_Two_Numbers/main.cpp
@@ -1,4 +1,39 @@
-#include <stdio.h>
+#include <iostream>
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x): val(x), next(NULL){}
+};
+
+typedef struct ListNode ListNode;
+
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+	ListNode *first = new ListNode(0), *p = first;
+	int c = 0;
+	
+	ListNode *p1, *p2;
+	for (p1 = l1, p2 = l2; p1 && p2; p1=p1->next, p2=p2->next) {
+		int k = p1->val + p2->val + c;
+		ListNode *l = new ListNode(k%10);
+		p->next = l; p = l;
+		c = k / 10;		
+	}
+	ListNode *r = p1? p1: p2;
+	if (r || c) {
+		for (; r; r = r->next) {
+			int k = r->val + c;
+			ListNode *l = new ListNode(k%10);
+			p->next = l; p = l;
+			c = k / 10;
+		}
+		if (c) {
+			ListNode *l = new ListNode(c);
+			p->next = l;
+		}
+	}
+	return first->next;
+}
 
 int main(int argc, char **argv)
 {
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ Binary_Tree_Postorder_Traversal - Debug ]----------"
-	@cd "Binary_Tree_Postorder_Traversal" && $(MAKE) -f  "Binary_Tree_Postorder_Traversal.mk"
+	@echo "----------Building project:[ Add_Two_Numbers - Debug ]----------"
+	@cd "Add_Two_Numbers" && $(MAKE) -f  "Add_Two_Numbers.mk"
 clean:
-	@echo "----------Cleaning project:[ Binary_Tree_Postorder_Traversal - Debug ]----------"
-	@cd "Binary_Tree_Postorder_Traversal" && $(MAKE) -f  "Binary_Tree_Postorder_Traversal.mk" clean
+	@echo "----------Cleaning project:[ Add_Two_Numbers - Debug ]----------"
+	@cd "Add_Two_Numbers" && $(MAKE) -f  "Add_Two_Numbers.mk" clean
